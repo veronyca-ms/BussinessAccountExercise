1b62180efd9aadb3132f9a1568dd69f35cec11f6
veronyca-ms <veronicasantos7262@gmail.com>
2025-03-24 14:24:57 -0300

    Adicionar arquivos de projeto.

---
+++ BussinessAccountExercise/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿namespace BussinessAccountExercise.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; } //protected é para proteger e poder usar apenas em subclasses
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
---
+++ BussinessAccountExercise/Entities/BussinessAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessAccountExercise.Entities
+{
+        class BusinessAccount : Account  //adicionando todos os atributos de Account em BussinessAccount
+        {
+            public double LoanLimit { get; set; }
+
+            public BusinessAccount()
+            {
+            }
+
+            public BusinessAccount(int number, string holder, double balance, double loanLimit)
+                : base(number, holder, balance)
+            {
+                LoanLimit = loanLimit;
+            }
+
+            public void Loan(double amount)
+            {
+                if (amount <= LoanLimit)
+                {
+                    Balance += amount;
+                }
+            }
+        }
+
+}
